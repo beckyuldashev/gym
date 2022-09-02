5252a217954ddf2f8208d295ead2b20c4d6fd865
Beck Yuldashev <yuldashev.bek@gmail.com>
2022-09-02 20:34:57 +0300

    Add header, nav and styling them

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 /*=============== SHOW MENU ===============*/
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+const navClose = document.getElementById('nav-close');
 
+const openMenu = () => {
+  navMenu.classList.add('show-menu');
+};
+
+const closeMenu = () => {
+  navMenu.classList.remove('show-menu');
+};
+
+if(navToggle) {
+  navToggle.addEventListener('click', openMenu);
+}
+
+if(navClose) {
+  navClose.addEventListener('click', closeMenu);
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll('.nav__link');
 
+const linkAction = () => {
+  navMenu.classList.remove('show-menu');
+};
+
+navLinks.forEach(link => link.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
