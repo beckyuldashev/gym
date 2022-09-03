8c526ce4674e60db04c7e36892deebae8a212d0a
Beck Yuldashev <yuldashev.bek@gmail.com>
2022-09-03 12:36:05 +0300

    Add scripts for changing header bg

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,7 +29,13 @@ const linkAction = () => {
 navLinks.forEach(link => link.addEventListener('click', linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById('header');
 
+  this.scrollY > 50 ? header.classList.add('scroll-header') : header.classList.remove('scroll-header');
+};
+
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
